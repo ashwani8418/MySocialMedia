d1cf464de1035e730c35a4e7deee5d9270d3b171
ashwani8418 <68030476+ashwani8418@users.noreply.github.com>
2023-10-14 03:49:00 +0530

    user implementation

--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ import { fileURLToPath } from 'url';
 import {register} from './controllers/auth.js';
 
 import authRoutes from './routes/auth.js';
+import userRoutes from './routes/users.js';
 
 
 /* CONFIGURATIONS*/ 
@@ -49,6 +50,7 @@ app.post("/auth/register", upload.single("picture"), register);
 /* ROUTES */
 
 app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
 
 
 /* MONGOOSE SETUP */
