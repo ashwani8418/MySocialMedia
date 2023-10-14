5c585982a0614a7eebc044fd187f6791eb295f71
ashwani8418 <68030476+ashwani8418@users.noreply.github.com>
2023-10-15 04:00:43 +0530

    Home, Login Page Created

--- client/src/Pages/loginPage/index.jsx
+++ client/src/Pages/loginPage/index.jsx
@@ -1,4 +1,6 @@
 import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
+import Form from "./Form";
+
 
 const LoginPage = () => {
   const theme = useTheme();
@@ -17,9 +19,9 @@ const LoginPage = () => {
       backgroundColor={theme.palette.background.alt}
       >
         <Typography fontWeight="500" variant="h5" sx={{mb : "1.5rem"}}>
-
+            Welcome to Sociopedia, the Social Media for Sociopaths
         </Typography>
-
+        <Form />
       </Box>
     </Box>
   );
