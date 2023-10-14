d2ca76183c2ebb94b1b0b71123404c0faac70609
ashwani8418 <68030476+ashwani8418@users.noreply.github.com>
2023-10-14 17:50:41 +0530

    Fronted SetupDone

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,18 @@
-import "./App.css";
+import HomePage from 'Pages/homePage';
+import LoginPage from 'Pages/loginPage';
+import ProfilePage from 'Pages/profilePage';
+import{ BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/home" element={<HomePage />} />
+      <Route path="/profile/:userId" element={<ProfilePage />} />
+    </Routes>
+    </BrowserRouter>
+  </div>;
 }
 
 export default App;
